a3ea4d760021eded6a3522e68a7a38ec07b64943
anmerkning <>
2025-11-11 09:56:24 +0100

    Changed title, updated base in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/interactive-canvas-portrait",
   plugins: [react(), tailwindcss()],
 });
